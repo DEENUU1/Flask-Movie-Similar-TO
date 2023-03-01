a058b361ae0b866491a8822e1aec71fc403a16b4
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-01 19:52:56 +0100

    Returning all similar movies

--- similar_movies/search_movie.py
+++ similar_movies/search_movie.py
@@ -6,18 +6,34 @@ import json
 load_dotenv()
 
 
-def create_query(query="Ant man and the wasp"):
+def create_query(query="Mission impossible"):
     query_list = query.lower().split()
     return '-'.join(query_list)
 
 
-def return_data():
+def return_movie_id():
     api_key = os.getenv("MOVIEDB_API_KEY")
-    base_url = f"https://api.themoviedb.org/3/search/movie?api_key="
+    base_url = "https://api.themoviedb.org/3/search/movie?api_key="
     result = get(base_url + api_key + "&query=" + create_query())
     json_result = json.loads(result.content)
-    print(base_url + api_key + "&query=" + create_query())
-    return json_result
+    movie = json_result['results']
+
+    if result.status_code == 200:
+        return str(movie[0]['id'])
+    else:
+        raise Exception("Error")
 
 
-print(return_data())
+def search_similar_movie():
+    api_key = os.getenv("MOVIEDB_API_KEY")
+    base_url = "https://api.themoviedb.org/3/movie/"
+    result = get(base_url + return_movie_id() + "/similar?api_key=" + api_key)
+    json_result = json.loads(result.content)
+    similar_movies = json_result['results']
+
+    if result.status_code == 200:
+        return [movie for movie in similar_movies]
+    else:
+        raise Exception("Error")
+
+print(search_similar_movie())
