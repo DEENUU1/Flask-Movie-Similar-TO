d64e8ac2560b3fa36e4fa3356bc1b926e9ee4e3d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-01 21:31:38 +0100

    Object oriented code, change similar to recomendations

--- similar_movies/search_movie.py
+++ similar_movies/search_movie.py
@@ -2,38 +2,77 @@ from requests import get
 from dotenv import load_dotenv
 import os
 import json
+from dataclasses import dataclass
 
 load_dotenv()
 
 
-def create_query(query="Mission impossible"):
-    query_list = query.lower().split()
-    return '-'.join(query_list)
+class SearchMovie:
+    """ This class allows to get movie ID """
+
+    def __init__(self, query: str = "Avengers"):
+        self.api_key = os.getenv('MOVIEDB_API_KEY')
+        self.query = query
+
+    def create_query(self) -> str:
+        """ This method format the user input into query """
+        query_list = self.query.lower().split()
+        return '-'.join(query_list)
+
+    @property
+    def return_movie_id(self):
+        """ This method returns user movie ID """
+        base_url = "https://api.themoviedb.org/3/search/movie?api_key="
+        result = get(base_url + self.api_key + "&query=" + self.create_query())
+        print(base_url + self.api_key + "&query=" + self.create_query())
+        json_result = json.loads(result.content)
+        movie = json_result['results']
+
+        if result.status_code == 200:
+            return str(movie[0]['id'])
+        else:
+            raise Exception("Error")
 
 
-def return_movie_id():
-    api_key = os.getenv("MOVIEDB_API_KEY")
-    base_url = "https://api.themoviedb.org/3/search/movie?api_key="
-    result = get(base_url + api_key + "&query=" + create_query())
-    json_result = json.loads(result.content)
-    movie = json_result['results']
-
-    if result.status_code == 200:
-        return str(movie[0]['id'])
-    else:
-        raise Exception("Error")
+@dataclass()
+class SimilarMovieData:
+    title: str
+    overview: str
+    release_date: str
+    poster: str
 
 
-def search_similar_movie():
-    api_key = os.getenv("MOVIEDB_API_KEY")
-    base_url = "https://api.themoviedb.org/3/movie/"
-    result = get(base_url + return_movie_id() + "/similar?api_key=" + api_key)
-    json_result = json.loads(result.content)
-    similar_movies = json_result['results']
+class SimilarMovies:
 
-    if result.status_code == 200:
-        return [movie for movie in similar_movies]
-    else:
-        raise Exception("Error")
+    def __init__(self):
+        self.search = SearchMovie()
 
-print(search_similar_movie())
+    def search_for_similar_movies(self) -> json:
+        """ This method is returning json file with similar movies"""
+        api_key = os.getenv("MOVIEDB_API_KEY")
+        base_url = "https://api.themoviedb.org/3/movie/"
+        response = get(f"{base_url}{self.search.return_movie_id}/recommendations?api_key={api_key}&language=us")
+        json_result = json.loads(response.content)
+        return json_result['results']
+
+    def return_similar_movies(self) -> list[SimilarMovieData]:
+        """ This method returns
+            title, release date, overview, photo
+            for all similar movies"""
+        all_movies = []
+        for movie in self.search_for_similar_movies():
+            movie_data = SimilarMovieData(
+                title=movie['title'],
+                release_date=movie['release_date'],
+                overview=movie['overview'],
+                poster=movie['poster_path']
+            )
+            all_movies.append(movie_data)
+        return all_movies
+
+
+obj = SimilarMovies()
+movies_similar = obj.return_similar_movies()
+
+for movie in movies_similar:
+    print(movie.title)
