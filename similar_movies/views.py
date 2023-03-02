274c03f29567550d41da737afd1161f2e19fca4f
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-02 12:36:36 +0100

    Main view to search movie and display similar movies

--- similar_movies/views.py
+++ similar_movies/views.py
@@ -1,23 +1,23 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for
 from similar_movies.search_movie import SimilarMovies, SearchMovie
 
 
 views = Blueprint('views', __name__)
 
 
-# @views.route('/', methods=['POST', 'GET'])
-# def home():
-#     if request.method == 'POST':
-#         movie_name = request.POST['movie_name']
-#         return movie_name
-#     else:
-#         return render_template('home.html')
-#     return redirect()
+@views.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        movie_name = request.form['movie_name']
+        return redirect(url_for('views.list_similar_movie', movie_name=movie_name))
+    else:
+        return render_template('home.html')
 
 
 @views.route('/similar', methods=['GET'])
 def list_similar_movie():
-    similar_movies = SimilarMovies(home())
+    movie_name = request.args.get("movie_name")
+    similar_movies = SimilarMovies(movie_name)
     return_similar_movies = similar_movies.return_similar_movies()
 
     return render_template('list_similar_movie.html',
