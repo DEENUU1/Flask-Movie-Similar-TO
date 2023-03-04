51bd14c9a68e9e0aab6bcfb49e94b677ed26cd93
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-04 22:00:38 +0100

    Tests for models

--- similar_movies/tests/test_views.py
+++ similar_movies/tests/test_views.py
@@ -1,5 +1,9 @@
 from similar_movies import create_app
-
+from similar_movies import db
+from similar_movies.models import User
+import pytest
+from werkzeug.security import generate_password_hash
+from similar_movies import create_app
 
 def test_home_view():
     """ Test for the main view status code
@@ -19,3 +23,22 @@ def test_list_similar_movies_view():
     with flask_app.test_client() as test_client:
         response = test_client.get('/similar', query_string={'movie_name': 'The Matrix'})
         assert response.status_code == 200
+
+@pytest.fixture
+def logged_in_client():
+    # create a test user
+    user = User(username='testuser', password=generate_password_hash('testpassword'))
+    db.session.add(user)
+    db.session.commit()
+
+    # log in the user using the test client
+    with app.test_client() as client:
+        client.post('/login', data={'username': 'testuser', 'password': 'testpassword'})
+        with client.session_transaction() as session:
+            session['_user_id'] = user.id
+        yield client
+
+    # clean up the test user
+    db.session.delete(user)
+    db.session.commit()
+
