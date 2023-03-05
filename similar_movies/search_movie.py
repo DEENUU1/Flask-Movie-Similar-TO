860cbc64f05eb16a08c34b57f8d75a9653f70aaf
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-05 02:47:28 +0100

    Returning all recomended tv shows

--- similar_movies/search_movie.py
+++ similar_movies/search_movie.py
@@ -9,7 +9,7 @@ from typing import List, Dict, Any
 load_dotenv()
 
 
-class SearchMovie:
+class Search:
     """ This class allows to get movie ID """
 
     def __init__(self, query: str):
@@ -22,7 +22,7 @@ class SearchMovie:
         return '-'.join(query_list)
 
     @property
-    def return_movie_id(self):
+    def return_movie_id(self) -> str:
         """ This method returns user movie ID """
         base_url = "https://api.themoviedb.org/3/search/movie?api_key="
         result = get(base_url + self.api_key + "&query=" + self.create_query())
@@ -33,6 +33,65 @@ class SearchMovie:
             return str(movie[0]['id'])
         else:
             raise Exception("Error")
+
+    @property
+    def return_tv_show_id(self) -> str:
+        """ This method returns user tv show ID """
+        base_url = "https://api.themoviedb.org/3/search/tv?api_key="
+        result = get(base_url + self.api_key + "&query=" + self.create_query())
+        json_result = json.loads(result.content)
+        tv_show = json_result["results"]
+
+        if result.status_code == 200:
+            return str(tv_show[0]['id'])
+        else:
+            raise Exception("Error")
+
+
+@dataclass()
+class SimilarTvShowData:
+    title: str
+    overview: str
+    first_air_date: str
+    poster: str
+
+
+class SimilarTvShows:
+    def __init__(self, query: str):
+        self.search = Search(query)
+
+    def search_for_similar_tv_shows(self) -> List[Dict[str, Any]]:
+        """ This method is searching for similar tv shows"""
+        api_key = os.getenv("MOVIEDB_API_KEY")
+        base_url = "https://api.themoviedb.org/3/tv/"
+        all_results = []
+        page_number = 1
+        while True:
+            response = get(
+                f"{base_url}{self.search_for_similar_tv_shows}/recomendations?api_key={api_key}&page={page_number}")
+            json_result = json.loads(response.content)
+            if not json_result.get('results'):
+                break
+            all_results.extend(json_result['results'])
+            page_number += 1
+            if page_number > 1000:
+                break
+        return all_results
+
+    def return_similar_movies(self) -> list[SimilarTvShowData]:
+        """ This method returns
+            title, first air date, overview, photo
+            for all similar tv shows"""
+        all_tv_shows = []
+        for shows in self.search_for_similar_tv_shows():
+            shows_data = SimilarTvShowData(
+                title=shows['title'],
+                first_air_date=shows['first_air_date'][:4],
+                overview=shows['overview'],
+                poster=shows['poster_path']
+            )
+            all_tv_shows.append(shows_data)
+        return all_tv_shows
 
 
 @dataclass()
@@ -46,7 +105,7 @@ class SimilarMovieData:
 class SimilarMovies:
 
     def __init__(self, query: str):
-        self.search = SearchMovie(query)
+        self.search = Search(query)
 
     def search_for_similar_movies(self) -> List[Dict[str, Any]]:
         """ This method is returning json file with similar movies
